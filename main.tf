34b85e9bfde88f2ca8c7c321a3b06329495ea109
akasshdeep9 <102315432+akasshdeep9@users.noreply.github.com>
2022-03-31 16:38:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,11 +68,11 @@ resource "kubernetes_deployment" "color" {
                     }//port          
                     
                     resources {
-                        limits {
+                        limits={
                             cpu    = "0.5"
                             memory = "512Mi"
                         } //limits
-                        requests {
+                        requests={
                             cpu    = "250m"
                             memory = "50Mi"
                         } //requests
